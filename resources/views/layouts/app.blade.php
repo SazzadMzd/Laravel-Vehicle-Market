31c15593956fef758eaee05312a1d24254ba37cd
Sazzad <sazzad.mzd@gmail.com>
2025-06-14 12:59:33 +0600

    14/06/25
    started creating components

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.clean')
+
+@section('childContent')
+     @include('layouts.partials.header')
+    @yield('content')
+
+
+    @hasSection('footerLinks')
+    <footer>
+       @section('footerLinks')
+       <a href="#">Link 1</a>
+       <a href="#">Link 2</a>
+       @show
+       
+    </footer>
+    @endif
+@endsection
+    
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
